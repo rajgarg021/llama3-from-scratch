a98981824109582ed716f9776160f7de7b0e94b6
rajgarg021 <rajgarg021@gmail.com>
2024-04-25 18:59:59 +0530

    added text_completion method for generating new tokens for a list of prompts

--- inference.py
+++ inference.py
@@ -2,9 +2,12 @@ import torch
 import time
 import json
 from pathlib import Path
+from typing import Optional
+from tqdm import tqdm
 
 from model import ModelArgs, Transformer
 from tokenizer import Tokenizer
+
 
 class Llama:
     """ Llama class """
@@ -76,8 +79,82 @@ class Llama:
         next_token = torch.gather(probs_idx, -1, next_token) 
         return next_token
 
+    @torch.inference_mode()
+    def text_completion(self, prompts: list[str], temperature: float = 0.6, top_p: float = 0.9, max_gen_len: Optional[int] = None):
+        """ Generating new tokens and performing text completion for a list of prompts """
 
+        params = self.model.params
 
+        # if maximum length of the generated completion sequence is not provided, it's set to the model's maximum sequence length minus 1
+        if max_gen_len is None:
+            max_gen_len = params.max_seq_len - 1
+
+        # making sure the batch size is not too large
+        batch_size = len(prompt_tokens)
+        assert batch_size <= params.max_batch_size, f"batch size must be less than or equal to {params.max_batch_size}"
+
+        # converting each prompt into tokens
+        prompt_tokens = [self.tokenizer.encode(prompt, bos=True, eos=False) for prompt in prompts]
+        # making sure the maximum prompt length is not larger than the maximum sequence length
+        max_prompt_len = max(len(prompt) for prompt in prompt_tokens)
+        assert max_prompt_len <= params.max_seq_len, f"prompt length must be less than or equal to {params.max_seq_len}"
+        # total number of tokens we want to generate using the model
+        total_len = min(params.max_seq_len, max_gen_len + max_prompt_len)
+
+        # creating a list that will contain the generated tokens, along with the initial prompt tokens
+        pad_id = self.tokenizer.pad_id()
+        tokens = torch.full((batch_size, total_len), pad_id, dtype=torch.long, device=device)
+        for k, t in enumerate(prompt_tokens):
+            # populating the initial tokens with the prompt tokens
+            tokens[k, : len(t)] = torch.tensor(t, dtype=torch.long, device=device)
+        
+        # variable that tell us if we reached the end of sentence in any of the prompts
+        eos_reached = torch.tensor([False] * batch_size, device=device)
+        # True if the token is a prompt token, False otherwise
+        prompt_tokens_mask = tokens != pad_id
+
+        stop_tokens = torch.tensor(list(self.tokenizer.stop_tokens))
+        cur_iterator = tqdm(range(1, total_len), desc="Generating tokens")
+        for cur_pos in cur_iterator:
+            with torch.no_grad():
+                logits = self.model.forward(tokens[:, cur_pos-1:cur_pos], cur_pos)
+            if temperature > 0:
+                # applying temperature before softmax
+                probs = torch.softmax(logits[:, -1] / temperature, dim=-1)
+                next_token = self._sample_top_p(probs, top_p)
+            else:
+                # greedily selecting the token with the max probability
+                next_token = torch.argmax(logits[:, -1], dim=-1)
+
+            next_token = next_token.reshape(-1)
+            # replacing the token if it is a padding token
+            next_token = torch.where(prompt_tokens_mask[:, cur_pos], tokens[:, cur_pos], next_token)
+            tokens[:, cur_pos] = next_token
+
+            # EOS is reached only if we find a stop token in a padding position
+            eos_reached |= (~prompt_tokens_mask[:, cur_pos]) & (torch.isin(next_token, stop_tokens))
+            if all(eos_reached):
+                break
+
+        out_tokens, out_text = [], []
+        for prompt_index, current_prompt_tokens in enumerate(tokens.tolist()):
+            # cutting to max gen len
+            start = len(prompt_tokens[prompt_index])
+            current_prompt_tokens = current_prompt_tokens[start : len(prompt_tokens[prompt_index]) + max_gen_len]
+            probs = None
+
+            # cutting to after eos tok if any
+            for stop_token in self.tokenizer.stop_tokens:
+                eos_idx = current_prompt_tokens.index(stop_token)
+                current_prompt_tokens = current_prompt_tokens[:eos_idx]
+
+            out_tokens.append(current_prompt_tokens)
+            out_text.append(self.tokenizer.decode(current_prompt_tokens))
+
+        return (out_tokens, out_text)
+    
+
+# inferencing the model
 if __name__ == "__main__":
     torch.manual_seed(42)
 
@@ -85,7 +162,22 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available and allow_cuda else "cpu"
 
     prompts = [
-        ""
+        "In a nutshell, self attention is the ",
+        "If there were 30 hours in a day, then",
+
+        # few shot prompt
+        """Translate English to French:
+
+        clouds -> des nuages
+        ice cream -> glace
+        dog -> chien
+        cheese ->""",
+
+        # zero shot prompt
+        """Do you think the following person is the best batsman in the world?
+        Name: Virat Kohli
+        Answer: 
+        """
     ]
 
     model = Llama.build(
@@ -97,4 +189,8 @@ if __name__ == "__main__":
         device=device
     )
 
-print("ALL GOOD! :)")
+    out_tokens, out_texts = (model.text_completion(prompts, max_gen_len=64))
+    assert len(out_texts) == len(prompts)
+    for i in range(len(out_texts)):
+        print(f'{out_texts[i]}')
+        print('-' * 50)

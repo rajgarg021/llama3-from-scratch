fafc470e4fa9887e29a993695d76ab3a5c40cbe5
rajgarg021 <rajgarg021@gmail.com>
2024-04-25 18:11:30 +0530

    added a method for performing top-p (nucleus) sampling on a given probability distribution

--- inference.py
+++ inference.py
@@ -9,9 +9,8 @@ from tokenizer import Tokenizer
 class Llama:
     """ Llama class """
 
-    def __init__(self, model: Transformer, model_args: ModelArgs, tokenizer: Tokenizer):
+    def __init__(self, model: Transformer, tokenizer: Tokenizer):
         self.model = model
-        self.args = model_args
         self.tokenizer = tokenizer
 
     @staticmethod
@@ -60,6 +59,23 @@ class Llama:
 
         return Llama(model, model_args, tokenizer)
     
+    def _sample_top_p(self, probs, p):
+        """ Performing top-p (nucleus) sampling on a given probability distribution """
+
+        probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True) # (B, vocab_size)
+        probs_sum = torch.cumsum(probs_sort, dim=-1) # (B, vocab_size)
+        # substracting "probs_sort" shifts the cumulative sum by 1 position to the right before masking
+        mask = probs_sum - probs_sort > p # (B, vocab_size)
+        # zeroing out all the probabilities of tokens that are not selected by the top p
+        probs_sort[mask] = 0.0 
+        # redistributing the probabilities so that they sum up to 1
+        probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
+        # sampling a token (its index) from the top p distribution
+        next_token = torch.multinomial(probs_sort, num_samples=1)
+        # gathering the token position in the vocabulary corresponding to the sampled index
+        next_token = torch.gather(probs_idx, -1, next_token) 
+        return next_token
+
 
 
 if __name__ == "__main__":
